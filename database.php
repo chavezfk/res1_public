5edf85cd2dd787108fbcf1b62f8c193a17c9afa6
Franz Chavez <chavezfk@gmail.com>
2017-03-08 10:48:07 -0700

    change to use double quote and pass by reference errors

--- database.php
+++ database.php
@@ -3,13 +3,15 @@
 // these are the MySQL database credentials we use
 // please set them to wherever the database may be found
 
-$DB_HOST     = 'localhost';
-$DB_USER     = 'skeen';
-$DB_PASSWORD = 'skeen';
-$DB_NAME     = 'skeen';
+$DB_HOST     = "localhost";
+$DB_USER     = "skeen";
+$DB_PASSWORD = "skeen";
+$DB_NAME     = "skeen";
+$DB_STR      = "mysql:host=$DB_HOST;dbname=$DB_NAME;";
 
 require_once 'dbpdo.php';
 
-$DB = new PDO2DB(new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD));
+$D1 = new PDO($DB_STR, $DB_USER, $DB_PASSWORD);
+$DB = new PDO2DB($D1);
 
 ?>
